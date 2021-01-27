13dae837fb605ad7328c4b97b55bc5001cfd970b
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-27 14:43:55 -0700

    added a border to the bottom of the yearly savings model

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,8 +4,13 @@ interface Props {}
 
 export const Title = (props: Props) => {
 	return (
-		<h1 className="text-3xl text-gray-500 mb-4 font-bold tracking-wider">
-			Our Pricing
-		</h1>
+		<div className="flex flex-col justify-center items-center pb-2 border-b-2 border-solid border-purple-400">
+			<h1 className="text-3xl text-gray-500 mb-4 font-bold tracking-wider">
+				Our Pricing
+			</h1>
+			<h2 className="text-lg font-semibold text-gray-600">
+				Select yearly and save $5 a month!
+			</h2>
+		</div>
 	);
 };
