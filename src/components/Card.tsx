4083851574331a2785327bf1e219c60b108f582e
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-26 23:54:31 -0700

    added a weird state where I can utilize tailwind with state to give me the colors I want

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Perks } from "./Perks";
 import { Button } from "./Button";
 import { PricingCard } from "./Cards";
@@ -8,13 +8,27 @@ interface Props {
 }
 
 export const Card: React.FC<Props> = ({ card }) => {
-	const [isPrimary, setisPrimary] = useState(card.isPrimary);
+	const [colors, setColors] = useState({
+		bgColor: "gray-50",
+		txtColor: "gray-600",
+		btnColor: "purple-400",
+		btnTxtColor: "gray-100",
+	});
+
+	useEffect(() => {
+		if (card.isPrimary) {
+			setColors({
+				bgColor: "purple-400",
+				txtColor: "gray-100",
+				btnColor: "gray-50",
+				btnTxtColor: "gray-600",
+			});
+		}
+	}, []);
 
 	return (
 		<div
-			className={`${
-				isPrimary ? "bg-purple-400 text-gray-100" : "bg-gray-50 text-gray-600"
-			} flex flex-col items-center justify-center m-4`}
+			className={`bg-${colors.bgColor} text-${colors.txtColor} flex flex-col items-center justify-center m-4`}
 		>
 			<h4 className="font-semibold">{card.model}</h4>
 			<h3 className="font-bold text-5xl">{`$${card.monthlyPrice}`}</h3>
