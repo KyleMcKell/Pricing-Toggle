ace72f54d968cd23ca680f5f71db75976306d34e
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-27 15:26:14 -0700

    made border dotted

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 export const Title = (props: Props) => {
 	return (
-		<div className="flex flex-col justify-center items-center pb-6 border-b-4 border-solid border-purple-400">
+		<div className="flex flex-col justify-center items-center pb-6 border-b-4 border-dotted border-purple-400">
 			<h1 className="text-3xl text-gray-600 mb-4 font-bold tracking-widest">
 				Our Pricing
 			</h1>
