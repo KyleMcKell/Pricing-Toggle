39eb8feca124c240b01f1d1eca40c48eaa5cb810
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-27 14:46:58 -0700

    updated bottom border in title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,8 +4,8 @@ interface Props {}
 
 export const Title = (props: Props) => {
 	return (
-		<div className="flex flex-col justify-center items-center pb-2 border-b-2 border-solid border-purple-400">
-			<h1 className="text-3xl text-gray-500 mb-4 font-bold tracking-wider">
+		<div className="flex flex-col justify-center items-center pb-6 border-b-4 border-solid border-purple-400">
+			<h1 className="text-3xl text-gray-600 mb-4 font-bold tracking-widest">
 				Our Pricing
 			</h1>
 			<h2 className="text-lg font-semibold text-gray-600">
